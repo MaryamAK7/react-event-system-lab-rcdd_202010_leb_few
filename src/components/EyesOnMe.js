42aab1ab05d86d1c285d9b7d18df9cb986993ee2
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-23 19:40:35 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+import React , {component} from 'react'
+
+export default class EyesOnMe extends React.Component {
+ render(){
+   return <div></div>
+ }
+ 
+  
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React , {component} from 'react'
+
+export default class Keypad extends React.Component {
+ constructor(){
+   super()
+   
+   
+ }
+ function handleKeyUp(e){
+   console.log('Entering password...')
+ }
+ render(){
+   return (
+     <div>
+     <input type="password" onKeyUp={this.handleKeyUp}/>
+     </div>
+     )
+ }
+ 
+  
+}
