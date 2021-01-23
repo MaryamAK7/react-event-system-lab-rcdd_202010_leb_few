ad3234659cb364f2c25e473b001241e6fa9506f9
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-23 19:42:46 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,10 +1,19 @@
 // Code EyesOnMe Component Here
-import React , {component} from 'react'
+import React from 'react';
 
-export default class EyesOnMe extends React.Component {
- render(){
-   return <div></div>
- }
- 
-  
+class EyesOnMe extends React.Component {
+
+  onFocus = () => console.log('Good!')
+
+  onBlur = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return (
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
+      </button>
+    )
+  }
 }
+
+export default EyesOnMe;
